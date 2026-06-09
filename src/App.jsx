841199d85d44b1cab39940f2ef9257808c246c96
Prince <mauryaprince379@gmail.com>
2026-06-09 15:20:27 +0530

    Feat: controlled inputs at auth page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { useState } from 'react'
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
 import LoginPage from './Pages/LoginPage.jsx';
 import SignupPage from './Pages/SignupPage.jsx';
 import './LoginPage.css';
@@ -8,12 +7,10 @@ function App() {
   
 
   return (
-    <Router>
       <Routes>
         <Route path="/signup" element={<SignupPage mode="signup"/>}/>
         <Route path="/login" element={<LoginPage mode="login"/>}/>      
       </Routes>
-    </Router>
     
     //understand browser router, routes etc
   )
--- src/Components/LoginPageComponents/AuthCard.jsx
+++ src/Components/LoginPageComponents/AuthCard.jsx
@@ -1,17 +1,25 @@
+import {useState} from 'react';
 import {Link} from 'react-router-dom';
 
 function AuthCard ({mode}){ 
     const isSignup = mode === "signup";
+    let [username, setUsername] = useState('');
+    let [email, setEmail] = useState('');
+    let [password, setPassword] = useState('');
+    let [confirmPassword,setConfirmPassword] = useState('');
+
+    const matchPassword = password === confirmPassword;
     return (
         <div className="rightPart w-1/2  max-lg:min-w-screen flex items-center justify-center bg-taupe-50 p-8">
-                <form className="flex flex-col items-left justify-center bg-gray-400 px-8 py-12  rounded-[20px] shadow-lg" required>
-                   {isSignup && <input type="text" placeholder="Name" className="w-[350px] border-1 rounded-[10px] border-black-300 focus:outline-none h-[50px]  bg-gray-50 p-4" />}
+                <form className="flex flex-col items-left justify-center bg-gray-400 px-8 py-12  rounded-[20px] shadow-lg" >
+                   {isSignup && <input required value={username} onChange={(e)=>{setUsername(e.target.value);console.log('username updated')}} type="text" placeholder="Name" className="w-[350px] border-1 rounded-[10px] border-black-300 focus:outline-none h-[50px]  bg-gray-50 p-4" />}
 
-                    <input type="text" placeholder="Email" className="w-[350px] border-1 rounded-[10px] border-black-300 focus:outline-none h-[50px]  bg-gray-50 p-4 mt-4" />
+                    <input required value={email} onChange={(e)=>{setEmail(e.target.value);console.log('email updated')}} type="text" placeholder="Email" className="w-[350px] border-1 rounded-[10px] border-black-300 focus:outline-none h-[50px]  bg-gray-50 p-4 mt-4" />
                     
-                    <input type="password" placeholder="Password" className="w-[350px] border border-black-300 focus:outline-none bg-gray-50 p-4 rounded-[10px] mt-4 h-[50px]" />
+                    <input required value={password} onChange={(e)=>{setPassword(e.target.value);console.log('password updated')}} type="password" placeholder="Password" className="w-[350px] border border-black-300 focus:outline-none bg-gray-50 p-4 rounded-[10px] mt-4 h-[50px]" />
 
-                   {isSignup && <input type="password" placeholder="Confirm Password" className="w-[350px] border-1 rounded-[10px] border-black-300 focus:outline-none h-[50px]  bg-gray-50 p-4 mt-4" />}
+                   {isSignup && <input required value={confirmPassword} onChange={(e)=>{setConfirmPassword(e.target.value);console.log('confirmPassword updated')}} type="password" placeholder="Confirm Password" className={`w-[350px]  rounded-[10px] border-1  h-[50px]  bg-gray-50 p-4 mt-4 focus:outline-none
+                   ${matchPassword? 'border-black-300 ' : 'border-2 border-red-400 focus:ring-red-500 focus:outline-red  '}`}  />}
 
                     {!isSignup && <div className="forgotPass mt-2 text-sm ">
                         Forgot password? <Link to="#" className="text-blue-50 hover:underline">Click here</Link>
