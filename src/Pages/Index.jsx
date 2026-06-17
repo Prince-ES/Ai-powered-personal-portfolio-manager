77be04888a91fa10e13b5596d8b827c3e999612a
Prince <mauryaprince379@gmail.com>
2026-06-17 21:46:20 +0530

    manual slider added successfully on index page.

--- src/Pages/Index.jsx
+++ src/Pages/Index.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
-import {useState, useEffect} from 'react';
+import {useState, useEffect, useRef} from 'react';
+import {ChevronLeft, ChevronRight} from 'lucide-react';
 import Navbar from '../Components/Shared/Navbar';
 import '../Dashboard.css';
 import DashboardLogo from '../assets/DashboardLogo.svg';
@@ -10,21 +11,26 @@ import img4 from "../assets/indexPageImages/img4.png";
 import img5 from "../assets/indexPageImages/img5.png";
 
 function Index (){
-    
+    const intervalRef = useRef(null);
+    const restartTimer = ()=>{
+        clearInterval(intervalRef.current);
+        intervalRef.current = setInterval(()=>{
+            setIndex(prev=>((prev+1)%5));
+            console.log('over');
+        },5000) 
+    }
     const [index, setIndex] = useState(0);
     useEffect(()=>{
-       const interval = setInterval(()=>{
-            setIndex(prev=>((prev+1)%5))
-        },3000)
+        restartTimer();
 
-        return ()=>{clearInterval(interval)}
+        return ()=>{clearInterval(intervalRef.current)}
     },[])
     
     return (
         <div className="dashboard pt-8 fixed  m-0 ">
             <Navbar DashboardLogo={DashboardLogo} indexPage="indexPage"/>
-            <div className="content flex justify-between items-center min-w-screen p-8 max-lg:px-16 max-lg:flex-col-reverse max-lg:gap-8">
-                <div className=" relative text-white text-5xl max-lg:text-5xl z-3 w-1/2 max-lg:w-full bg-black/25 border rounded-[10px] py-16 px-8 ml-12 max-lg:ml-0 gap-8 max-lg:py-8 max-lg:px-8">
+            <div className="content flex justify-between items-center min-w-screen p-8 max-lg:px-16 max-lg:flex-col-reverse max-lg:gap-8 gap-8">
+                <div className=" relative text-white text-5xl max-lg:text-5xl z-3 w-1/2 max-lg:w-full bg-black/25 border rounded-[10px] py-16 px-8 ml-12 max-lg:ml-0 max-lg:py-8 max-lg:px-8">
                     <h1 className="font-bold mb-4">
                         Track - Analyze - Grow
                     </h1>
@@ -32,8 +38,11 @@ function Index (){
                         Your AI-powered portfolio manager that turns market data into actionable insights.
                     </h3>
                 </div>
-                <div className="insightsOverview overflow-hidden w-1/2 max-lg:w-full relative before:absolute before:left-0 after:content-[''] before:transform before:-translate-y-1/2 before:h-8 before:w-8 before:top-1/2 before:bg-white before:rotate-45 before:border-l-5 before:border-b-5 before:border-purple-600 before:z-4 before:left-20 after:content-[''] after:h-8 after:w-8 after:top-1/2 after:transform after:-translate-1/2 after:right-16 after:rotate-45 after:bg-white after:border-t-5 after:border-r-5 after:border-purple-600 after:absolute max-xl:after:right-8 after:z-[4] max-xl:before:left-12 max-md:before:left-4 max-md:after:right-0 max-lg:before:left-16 max-lg:after-right:16">
-                    <div className={`images w-full flex transform transition-all duration 0.15s relative z-3    `} style={{transform:`translateX(-${index*100}%)`}}>
+                <div className="insightsOverview flex items-center justify-center overflow-hidden w-1/2 max-lg:w-full relative ">
+                    <div className="leftChevron bg-white/25 absolute z-4 left-12 max-xl:left-0 max-lg:left-16 max-md:left-8 max-sm:left-0" onClick={()=>{setIndex(prev=>(prev-1+5)%5); clearInterval(intervalRef.current);restartTimer(); console.log('leftclick')}}>
+                        <ChevronLeft size={32}/>
+                    </div>                
+                    <div className={`images w-full flex transform transition-all duration-300 relative z-3    `} style={{transform:`translateX(-${index*100}%)`}}>
                         <div className="flex-shrink-0  w-full  flex items-center justify-center">
                             <img src={img1} className=" rounded-[10px]  h-[550px] w-[400px]  bg-blue-300" />
                         </div>
@@ -49,6 +58,9 @@ function Index (){
                         <div className="flex-shrink-0  w-full flex items-center justify-center">
                             <img src={img5} className=" rounded-[10px] h-[550px] w-[400px] bg-blue-300" />
                         </div>
+                    </div>
+                    <div className="rightChevron bg-white/25  absolute z-4 right-12 max-xl:right-0 max-lg:right-16 max-md:right-8 max-sm:right-0" onClick={()=>{setIndex(prev=>(prev+1)%5); clearInterval(intervalRef.current);restartTimer();}}>
+                        <ChevronRight size={32}/>
                     </div>
                 </div>
             </div>
