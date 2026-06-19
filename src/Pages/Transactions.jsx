7db6d4b702efa5f2ff29afb366b4111192d45a06
Prince <mauryaprince379@gmail.com>
2026-06-19 21:37:01 +0530

    Adding link to AddTransaction Page and fixing UI

--- src/Pages/Transactions.jsx
+++ src/Pages/Transactions.jsx
@@ -1,4 +1,5 @@
 import {useState} from 'react';
+import {Link} from 'react-router-dom';
 import '../Dashboard.css';
 import Navbar from '../Components/Shared/Navbar.jsx';
 import DashboardLogo from '../assets/DashboardLogo.svg';
@@ -17,16 +18,16 @@ function Transactions(){
                     <h1 className="headingTitle text-4xl font-bold text-white">
                         Transactions
                     </h1>
-                    <h2 className="addTransaction text-2xl text-white">
+                    <Link to="/addTransactionsPage"className="addTransaction text-2xl text-white">
                         + Add Transaction
-                    </h2>
+                    </Link>
                 </div>
                 <div className="inputBox w-[50%] flex max-xl:w-[60%] max-lg:w-[70%] max-md:w-[80%]">
                     <input type="text" value={searchInput} placeholder="Type to search..." className="w-full bg-white px-8 py-4 rounded-full text-xl outline-0" onChange={(e)=>{setSearchInput(e.target.value);console.log(e.target.value)}}/>
                 </div>
                 <div className="filters flex justify-center w-[50%] text-xl  gap-4">
                     <div className="categoryFilter">
-                        <select name="category" value={selectInput}className="outline-1 px-2 py-1 bg-white/25  rounded-[10px]" onChange={(e)=>{setSelectInput(e.target.value);console.log(e.target.value)}}>
+                        <select name="category" value={selectInput}className="outline-1 px-2 py-1 bg-white  rounded-[10px]" onChange={(e)=>{setSelectInput(e.target.value);console.log(e.target.value)}}>
                             <option value="">Category</option>
                             <option value="food" >Food</option>
                             <option value="travel">Travel</option>
@@ -34,10 +35,10 @@ function Transactions(){
                         </select>
                     </div>
                     <div className="Date">
-                        <input type="date" value={dateInput} className="outline-1 px-2 py-1 bg-white/35  rounded-[10px]"  onChange={(e)=>{setDateInput(e.target.value); console.log(e.target.value)}}/>
+                        <input type="date" value={dateInput} className="outline-1 px-2 py-1 bg-white  rounded-[10px]"  onChange={(e)=>{setDateInput(e.target.value); console.log(e.target.value)}}/>
                     </div>
                     <div className="amount ">
-                        <input type="number" value={amountInput} placeholder='Amount' className="outline-1  rounded-[10px] py-1 px-2 bg-white/50" onChange={(e)=>{setAmountInput(e.target.value); console.log(e.target.value)}}/>
+                        <input type="number" value={amountInput} placeholder='Amount' className="outline-1  rounded-[10px] py-1 px-2 bg-white" onChange={(e)=>{setAmountInput(e.target.value); console.log(e.target.value)}}/>
                     </div>
                 </div>
                 <div className="allTransactionsAndSearchResult px-8 py-4 rounded-[10px] flex justify-between bg-black/35 text-white w-[80%] border">
