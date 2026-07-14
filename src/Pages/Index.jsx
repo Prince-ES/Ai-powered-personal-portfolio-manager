86abc08864e733704b032b9c87631a8dba4099fb
Prince <mauryaprince379@gmail.com>
2026-07-14 21:26:38 +0530

    added hero content to Index page

--- src/Pages/Index.jsx
+++ src/Pages/Index.jsx
@@ -27,41 +27,50 @@ function Index (){
     
     return (
         <div className="dashboard pt-4 fixed  m-0 ">
-            <Navbar DashboardLogo={DashboardLogo} indexPage="indexPage"/>
-            <div className="content flex justify-between items-center min-w-screen p-8 max-lg:px-16 max-lg:flex-col-reverse max-lg:gap-8 gap-8">
-                <div className=" relative text-white text-5xl max-lg:text-5xl z-3 w-1/2 max-lg:w-full bg-black/25 border rounded-[10px] py-16 px-8 ml-12 max-lg:ml-0 max-lg:py-8 max-lg:px-8">
-                    <h1 className="font-bold mb-4">
-                        Track - Analyze - Grow
-                    </h1>
-                    <h3 className="text-2xl ">
-                        Your AI-powered portfolio manager that turns market data into actionable insights.
-                    </h3>
-                </div>
-                <div className="insightsOverview flex items-center justify-center overflow-hidden w-1/2 max-lg:w-full relative ">
-                    <div className="leftChevron bg-white/25 absolute z-4 left-12 max-xl:left-0 max-lg:left-16 max-md:left-8 max-sm:left-0" onClick={()=>{setIndex(prev=>(prev-1+5)%5); clearInterval(intervalRef.current);restartTimer(); console.log('leftclick')}}>
-                        <ChevronLeft size={32}/>
-                    </div>                
-                    <div className={`images w-full flex transform transition-all duration-300 relative z-3    `} style={{transform:`translateX(-${index*100}%)`}}>
-                        <div className="flex-shrink-0  w-full  flex items-center justify-center">
-                            <img src={img1} className=" rounded-[10px]  h-[550px] w-[400px]  bg-blue-300" />
-                        </div>
-                        <div className="flex-shrink-0  w-full flex items-center justify-center">
-                            <img src={img2} className=" rounded-[10px] h-[550px] w-[400px] bg-blue-300" />
-                        </div>
-                        <div className="flex-shrink-0  w-full flex items-center justify-center">
-                            <img src={img3} className=" rounded-[10px] h-[550px] w-[400px] bg-blue-300" />
-                        </div>
-                        <div className="flex-shrink-0  w-full flex items-center justify-center">
-                            <img src={img4} className=" rounded-[10px] h-[550px] w-[400px] bg-blue-300" />
-                        </div>
-                        <div className="flex-shrink-0  w-full flex items-center justify-center">
-                            <img src={img5} className=" rounded-[10px] h-[550px] w-[400px] bg-blue-300" />
-                        </div>
-                    </div>
-                    <div className="rightChevron bg-white/25  absolute z-4 right-12 max-xl:right-0 max-lg:right-16 max-md:right-8 max-sm:right-0" onClick={()=>{setIndex(prev=>(prev+1)%5); clearInterval(intervalRef.current);restartTimer();}}>
-                        <ChevronRight size={32}/>
+            <Navbar className="fixed w-full z-4" DashboardLogo={DashboardLogo} indexPage="indexPage"/>
+            <div className="content flex flex-col justify-between items-center min-w-screen p-8 max-lg:px-16 max-lg:flex-col-reverse max-lg:gap-8 gap-8 relative z-3">
+                <div className="h-screen w-full flex flex-col items-center justify-center">
+                    <div className="text-white p-8  flex flex-col items-center justify-center transform translate-y-[-13%] gap-16">
+                        <h1 className="gradientTitleIndex text-8xl font-bold text-center">Build Wealth with Confidence</h1>
+                        <h2 className="text-5xl font-bold text-center">Manage your income, expenses, investments, and financial goals from one intelligent dashboard powered by AI.</h2>
                     </div>
                 </div>
+                <div className="flex items-center">
+                    <div className=" text-white text-5xl max-lg:text-5xl w-1/2 max-lg:w-full bg-black/25 border rounded-[10px] py-16 px-8 ml-12 max-lg:ml-0 max-lg:py-8 max-lg:px-8">
+                        <h1 className="font-bold mb-4">
+                            Track - Analyze - Grow
+                        </h1>
+                        <h3 className="text-2xl ">
+                            Your AI-powered portfolio manager that turns market data into actionable insights.
+                        </h3>
+                    </div>
+                    <div className="insightsOverview flex items-center justify-center overflow-hidden w-1/2 max-lg:w-full relative ">
+                        <div className="leftChevron cursor-pointer bg-white/25 absolute z-4 left-12 max-xl:left-0 max-lg:left-16 max-md:left-8 max-sm:left-0" onClick={()=>{setIndex(prev=>(prev-1+5)%5); clearInterval(intervalRef.current);restartTimer(); console.log('leftclick')}}>
+                            <ChevronLeft size={32}/>
+                        </div>                
+                        <div className={`images w-full flex transform transition-all duration-300 relative z-3    `} style={{transform:`translateX(-${index*100}%)`}}>
+                            <div className="flex-shrink-0  w-full  flex items-center justify-center">
+                                <img src={img1} className=" rounded-[10px]  h-[550px] w-[400px]  bg-blue-300" />
+                            </div>
+                            <div className="flex-shrink-0  w-full flex items-center justify-center">
+                                <img src={img2} className=" rounded-[10px] h-[550px] w-[400px] bg-blue-300" />
+                            </div>
+                            <div className="flex-shrink-0  w-full flex items-center justify-center">
+                                <img src={img3} className=" rounded-[10px] h-[550px] w-[400px] bg-blue-300" />
+                            </div>
+                            <div className="flex-shrink-0  w-full flex items-center justify-center">
+                                <img src={img4} className=" rounded-[10px] h-[550px] w-[400px] bg-blue-300" />
+                            </div>
+                            <div className="flex-shrink-0  w-full flex items-center justify-center">
+                                <img src={img5} className=" rounded-[10px] h-[550px] w-[400px] bg-blue-300" />
+                            </div>
+                        </div>
+                        <div className="rightChevron cursor-pointer bg-white/25  absolute z-4 right-12 max-xl:right-0 max-lg:right-16 max-md:right-8 max-sm:right-0" onClick={()=>{setIndex(prev=>(prev+1)%5); clearInterval(intervalRef.current);restartTimer();}}>
+                            <ChevronRight size={32}/>
+                        </div>
+                    </div>
+                </div>
+                
             </div>
             
         </div>
